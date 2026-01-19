71715b40130b42cfd5e91771b2ca08ccce79dc9f
Maliklar <elbadrimalik@gmail.com>
2026-01-19 21:11:20 +0300

    Updated package.json

--- index.ts
+++
@@ -1,5 +0,0 @@
-import DBError from "./src/DBError";
-import DBStore from "./src/DBStore";
-import DBStoreIndex from "./src/DBStoreIndex";
-import BAsyncDB from "./src/index";
-export { DBError, DBStore, DBStoreIndex, BAsyncDB };
---
+++ src/AsyncDB.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import DBStore from "./DBStore";
+import IDbError from "./DBError";
+import type { IDbAsync } from "./types";
+
+export default class AsyncDB {
+  public static openReq: IDbAsync;
+  public static db: IDBDatabase;
+  public static dbName: string;
+  public static version?: number;
+  public static stores: DBStore<any>[];
+  public static storeNames: string[];
+
+  public static open(dbName: string, version?: number) {
+    this.dbName = dbName;
+    this.version = version;
+    this.stores = [];
+    return this;
+  }
+
+  public static addStore<T>(store: DBStore<T>) {
+    this.stores.push(store);
+    return this;
+  }
+
+  public static createStore<T>(
+    name: string,
+    options?: IDBObjectStoreParameters,
+  ) {
+    return new DBStore<T>(name, options);
+  }
+
+  public static async build(): Promise<IDBDatabase> {
+    this.openReq = window.indexedDB.open(this.dbName, this.version) as IDbAsync;
+    return new Promise((res, rej) => {
+      this.openReq.onerror = (e) => {
+        rej(new IDbError(e.target.error?.message, e));
+      };
+      this.openReq.onsuccess = (e) => {
+        this.db = e.target.result;
+        for (const store of this.stores) {
+          store.setDB(this.db);
+        }
+        res(this.db);
+      };
+      this.openReq.onupgradeneeded = (e) => {
+        this.db = e.target.result;
+        for (const store of this.stores) {
+          const s = this.db.createObjectStore(store.name, store.options);
+          const indexes = store.indexes;
+          for (const index of indexes) {
+            const name = index.name;
+            const keyPath = index.keyPath;
+            const options = index.options;
+            s.createIndex(name, keyPath, options);
+          }
+        }
+      };
+      this.openReq.onblocked = (e) => {
+        rej(new IDbError(e.target.error?.message, e));
+      };
+    });
+  }
+}
